bb5f4ac51c7f90e849314b50d221a1ee5b5ff263
Andrew Burnett <andrew@nospam.forme>
2015-11-02 16:01:11 +0100

    Add CLI stub

--- HearthstoneDisunityCLI/Program.cs
+++ HearthstoneDisunityCLI/Program.cs
@@ -10,6 +10,59 @@ namespace HearthstoneDisunityCLI
     {
         static void Main(string[] args)
         {
+            if (args.Length != 3)
+            {
+                PrintUsageAndExit();
+            }
+
+            var command = args[0];
+            var fileDir = args[1];
+            var outDir = args[2];
+
+            switch (command.ToLower())
+            {
+                case "extract":
+                    Extract(fileDir, outDir);
+                    break;
+                case "cardart":
+                    CardArt(fileDir, outDir);
+                    break;
+                default:
+                    PrintUsageAndExit();
+                    break;
+            }
+        }
+
+        private static void CardArt(string hsDir, string outDir)
+        {
+            Console.WriteLine("Extracting Card Art from {0} to {1}", hsDir, outDir);
+        }
+
+        private static void Extract(string file, string outDir)
+        {
+            Console.WriteLine("Extracting Assets from {0} to {1}", file, outDir);
+        }
+
+        private static bool IsValidFile(string file)
+        {
+            return false;
+        }
+
+        private static bool IsValidDir(string dir)
+        {
+            return false;
+        }
+
+        private static void PrintErrorAndExit(string message)
+        {
+            Console.WriteLine("Error: " + message);
+            Environment.Exit(1);
+        }
+
+        private static void PrintUsageAndExit()
+        {
+            Console.WriteLine("Usage: ");
+            Environment.Exit(1);
         }
     }
 }
