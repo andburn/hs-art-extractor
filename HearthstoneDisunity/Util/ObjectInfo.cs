d98671810580c80861e5f683c1d6f9531ee0fc2c
Andrew Burnett <andrew@nospam.forme>
2015-11-12 16:47:41 +0100

    Add asset object containers

---
+++ HearthstoneDisunity/Util/ObjectInfo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using HearthstoneDisunity.Unity;
+
+namespace HearthstoneDisunity.Util
+{
+	public class ObjectInfo
+	{
+		private int _version;
+
+		public long Offset { get; set; }
+		public long Length { get; set; }
+		public int TypeId { get; set; }
+		public short ClassId { get; set; }
+		public short IsDestroyed { get; set; }
+
+		public ObjectInfo(int version)
+		{
+			_version = version;
+		}
+
+		public void Read(BinaryFileReader b)
+		{
+			Offset = b.ReadUnsignedInt();
+			Length = b.ReadUnsignedInt();
+			TypeId = b.ReadInt();
+			ClassId = b.ReadShort();
+			IsDestroyed = b.ReadShort();
+			// TODO: the only way I can get this to work, align doesn't!
+			if(_version > 13)
+				b.ReadInt();
+
+			// sanity check
+			Debug.Assert(TypeId == ClassId || (ClassId == 114 && TypeId < 0));
+		}
+
+		public override string ToString()
+		{
+			return String.Format("[cid:{0} tid:{1}] {2}, {3}", ClassId, TypeId, Offset, Length);
+		}
+	}
+}
---
+++ HearthstoneDisunity/Util/ObjectInfoTable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HearthstoneDisunity.Util
+{
+	public class ObjectInfoTable
+	{
+		private int _version;
+		private Dictionary<long, ObjectInfo> _infoMap;
+		
+		public ObjectInfoTable(int version)
+		{
+			_version = version;
+		}
+
+		public void Read(BinaryFileReader b)
+		{
+			_infoMap = new Dictionary<long, ObjectInfo>();
+
+			int entries = b.ReadInt();
+
+			if(_version > 13)
+				b.Align(4);
+
+			for(int i = 0; i < entries; i++)
+			{
+				long pathID;
+				if (_version > 13)
+				{
+					pathID = b.ReadLong();
+				} 
+				else
+				{
+					pathID = b.ReadInt();
+				}
+
+				ObjectInfo info = new ObjectInfo(_version);
+				info.Read(b);
+				_infoMap.Add(pathID, info);
+			}
+		}
+
+		public Dictionary<long, ObjectInfo> InfoMap
+		{
+			get { return _infoMap; }
+		}
+
+		public int Size
+		{
+			get { return _infoMap.Count; }
+		}
+	}
+}
