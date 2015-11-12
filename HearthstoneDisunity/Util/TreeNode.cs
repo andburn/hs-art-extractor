ffa576404eb56ea02d7cca9257df63f84f26c194
Andrew Burnett <andrew@nospam.forme>
2015-11-12 11:24:11 +0100

    Add type tree structure

---
+++ HearthstoneDisunity/Util/TreeNode.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace HearthstoneDisunity.Util
+{
+	// Tree structure based on http://stackoverflow.com/a/10442244/2762059
+	public class TreeNode<T> where T : TreeNode<T>
+	{
+		private readonly List<T> _children = new List<T>();
+
+		public T Parent { get; private set; }
+
+		public T this[int i]
+		{
+			get { return _children[i]; }
+		}
+
+		public ReadOnlyCollection<T> Children
+		{
+			get { return _children.AsReadOnly(); }
+		}
+
+		public T AddChild(T node)
+		{
+			node.Parent = (T)this;
+			_children.Add(node);
+			return node;
+		}
+
+		public bool RemoveChild(T node)
+		{
+			return _children.Remove(node);
+		}
+
+		public TreeNode<T>[] AddChildren(params T[] values)
+		{
+			return values.Select(AddChild).ToArray();
+		}
+
+		public void Traverse(Action<TreeNode<T>> action)
+		{
+			action(this);
+			foreach(var child in _children)
+				child.Traverse(action);
+		}
+
+		public void TraverseChildren(Action<TreeNode<T>> action)
+		{
+			//action(this);
+			foreach(var child in _children)
+				child.Traverse(action);
+		}
+
+		//public IEnumerable<T> Flatten()
+		//{
+		//	return new[] { Value }.Union(_children.SelectMany(x => x.Flatten()));
+		//}
+
+		public bool IsEmpty()
+		{
+			return _children.Count <= 0;
+		}
+
+		public int Size()
+		{
+			return _children.Count;
+		}
+	}
+}
---
+++ HearthstoneDisunity/Util/TypeTree.cs
@@ -0,0 +1,93 @@
+﻿using HearthstoneDisunity.Unity;
+using HearthstoneDisunity.Unity.Objects;
+using System.Collections.Generic;
+
+namespace HearthstoneDisunity.Util
+{
+    public class TypeTree
+	{
+		private int _version;
+		private string _revision;
+		private Dictionary<int, BaseClass> _typeMap;
+		
+		public int Attributes { get; set; }
+		public bool Embedded { get; set; }
+
+		public TypeTree(int version)
+		{
+			_version = version;
+			_typeMap = new Dictionary<int, BaseClass>();
+		}
+
+		public Dictionary<int, BaseClass> TypeMap
+		{
+			get
+			{
+				if (_typeMap == null)
+					return new Dictionary<int, BaseClass>();
+				return _typeMap;
+			}
+		}
+
+		public void Read(BinaryFileReader b)
+		{
+			if(_version > 6)
+			{
+				_revision = b.ReadStringToNull();
+				Attributes = b.ReadInt();
+			}
+
+			if (_version > 13)
+			{
+				Embedded = b.ReadBoolean();
+				int numBaseClasses = b.ReadInt();
+
+				for (int i = 0; i < numBaseClasses; i++) {
+					int classID = b.ReadInt();
+					BaseClass baseClass = new BaseClass(classID);
+                
+					if (classID < 0)
+					{
+						UnityHash128 scriptID = new UnityHash128(_version);
+						scriptID.Read(b);
+						baseClass.ScriptID = scriptID;
+					}
+
+					UnityHash128 oldTypeHash = new UnityHash128(_version);
+					oldTypeHash.Read(b);
+					baseClass.OldTypeHash = oldTypeHash;
+
+					if (Embedded) {
+						AssetTypeNode node = new AssetTypeNode();
+						AssetTypeReader nodeReader = new AssetTypeReader(_version);
+						nodeReader.Read(b, node);
+						baseClass.TypeTree = node;
+					}
+                
+					_typeMap.Add(classID, baseClass);
+				}
+			}
+			else
+			{				
+				int numBaseClasses = b.ReadInt();
+
+				for(int i = 0; i < numBaseClasses; i++)
+				{
+					int classID = b.ReadInt();
+					BaseClass baseClass = new BaseClass(classID);
+
+					AssetTypeNode node = new AssetTypeNode();
+					AssetTypeReader nodeReader = new AssetTypeReader(_version);
+					nodeReader.Read(b, node);
+					baseClass.TypeTree = node;
+
+					_typeMap.Add(classID, baseClass);
+				}
+				Embedded = numBaseClasses > 0;
+				
+				if(_version > 6)
+					b.ReadInt(); // padding
+			}					
+		}
+	}
+}
