fbce0da77a4e467c53732b5da6ab17eb0876955d
Andrew Burnett <andrew@nospam.forme>
2015-12-09 14:45:58 +0000

    Refactor card art extraction out of Extract

--- HearthstoneDisunity/Extract.cs
+++ HearthstoneDisunity/Extract.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using HearthstoneDisunity.Hearthstone;
-using HearthstoneDisunity.Hearthstone.Bundle;
-using HearthstoneDisunity.Hearthstone.Xml;
 using HearthstoneDisunity.Unity;
 using HearthstoneDisunity.Unity.Extract;
 using HearthstoneDisunity.Util;
@@ -36,62 +32,8 @@ namespace HearthstoneDisunity
         // TODO: add set filter, include/exclude?
         public static void CardArt(string outDir, string hsDir)
         {
-            var hsDataPath = Path.Combine(hsDir, "Data", "Win");
-
-            if (!Directory.Exists(hsDataPath))
-            {
-                throw new DirectoryNotFoundException("The Hearthstone directory does not exist (" + hsDataPath + ")");
-            }
-
-            if (!Directory.Exists(outDir))
-            {
-                Directory.CreateDirectory(outDir);
-            }
-
-            Dictionary<string, List<CardArt>> map = new Dictionary<string, List<CardArt>>();
-
-            // extract card xml
-            var cardXml = Path.Combine(hsDataPath, "cardxml0.unity3d");
-            AssetBundle xmlBundle = new AssetBundle(cardXml);
-            // create a temp dir
-            var xmlDir = Directory.CreateDirectory(Path.Combine(outDir, "cardxml"));
-            //xmlBundle.ExtractText(xmlDir.FullName);
-            // TODO: check it exists
-            CardDb.Read(Path.Combine(xmlDir.FullName, "enUS.txt"));
-            var cards = CardDb.All;
-            Console.WriteLine("CardDB loaded: " + cards.Count);
-
-            // get all card defs (cards<n>.unity3d)
-            var cardFiles = Directory.GetFiles(hsDataPath, "cards?.unity3d");
-
-            foreach (var cf in cardFiles)
-            {
-                AssetBundle ab = new AssetBundle(cf);
-                CardsBundle cb = new CardsBundle(ab);
-
-                var m = cb.CardArtByTexture;
-                //ab.ExtractCards(outDir);
-                // Use this to join possible reuse of tex over bundles
-                foreach (var entry in m)
-                {
-                    if (!map.ContainsKey(entry.Key))
-                    {
-                        map[entry.Key] = new List<CardArt>();
-                    }
-                    map[entry.Key].AddRange(entry.Value);
-                }
-            }
-
-            // get all textures (cardtextures<n>.unity3d, shared<n>.unity3d)
-            var textureFiles = new List<string>(Directory.GetFiles(hsDataPath, "cardtextures?.unity3d"));
-            textureFiles.AddRange(Directory.GetFiles(hsDataPath, "shared?.unity3d"));
-
-            foreach (var tf in textureFiles)
-            {
-                AssetBundle ab = new AssetBundle(tf);
-                TexturesBundle tb = new TexturesBundle(ab, map);
-                tb.Extract(outDir);
-            }
+            var extractor = new CardArtExtractor(outDir, hsDir);
+            extractor.Extract();
         }
 
         private static void ExtractAssets(IExtractor extractor, string outDir, params string[] files)
---
+++ HearthstoneDisunity/Hearthstone/CardArtExtractor.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using HearthstoneDisunity.Hearthstone.Bundle;
+using HearthstoneDisunity.Hearthstone.Xml;
+using HearthstoneDisunity.Unity;
+using HearthstoneDisunity.Unity.Extract;
+using HearthstoneDisunity.Util;
+
+namespace HearthstoneDisunity.Hearthstone
+{
+    internal class CardArtExtractor
+    {
+        private string _hsDataPath;
+        private string _outDir;
+        private string _outDirRaw;
+
+        public CardArtExtractor(string outDir, string hsDir)
+        {
+            Logger.Log("Initializing CardArt ({0} to {1})", hsDir, outDir);
+
+            _hsDataPath = Path.Combine(hsDir, "Data", "Win");
+
+            if (!Directory.Exists(_hsDataPath))
+            {
+                throw new DirectoryNotFoundException("The Hearthstone directory does not exist (" + _hsDataPath + ")");
+            }
+
+            _outDir = outDir;
+
+            if (!Directory.Exists(outDir))
+            {
+                Directory.CreateDirectory(outDir);
+            }
+
+            _outDirRaw = Path.Combine(outDir, "Raw");
+            Directory.CreateDirectory(_outDirRaw);
+        }
+
+        public void Extract()
+        {
+            Dictionary<string, Card> cardDb = LoadCardDb();
+            Dictionary<string, List<CardArt>> cardArtMap = LoadCards();
+
+            // get all textures (cardtextures<n>.unity3d, shared<n>.unity3d)
+            var textureFiles = new List<string>(Directory.GetFiles(_hsDataPath, "cardtextures?.unity3d"));
+            textureFiles.AddRange(Directory.GetFiles(_hsDataPath, "shared?.unity3d"));
+
+            foreach (var tf in textureFiles)
+            {
+                AssetBundle ab = new AssetBundle(tf);
+                TexturesBundle tb = new TexturesBundle(ab, cardArtMap);
+                tb.Extract(_outDirRaw);
+            }
+        }
+
+        private Dictionary<string, List<CardArt>> LoadCards()
+        {
+            Dictionary<string, List<CardArt>> map = new Dictionary<string, List<CardArt>>();
+
+            // get all card defs (cards<n>.unity3d)
+            var cardFiles = Directory.GetFiles(_hsDataPath, "cards?.unity3d");
+
+            foreach (var cf in cardFiles)
+            {
+                AssetBundle ab = new AssetBundle(cf);
+                CardsBundle cb = new CardsBundle(ab);
+
+                var bundleMap = cb.CardArtByTexture;
+                // Use this to join possible reuse of tex over the x bundles
+                foreach (var entry in bundleMap)
+                {
+                    if (!map.ContainsKey(entry.Key))
+                    {
+                        map[entry.Key] = new List<CardArt>();
+                    }
+                    map[entry.Key].AddRange(entry.Value);
+                }
+            }
+
+            return map;
+        }
+
+        private Dictionary<string, Card> LoadCardDb()
+        {
+            Dictionary<string, Card> cardDb = new Dictionary<string, Card>();
+
+            // Extract card xml files
+            var cardXml = Path.Combine(_hsDataPath, "cardxml0.unity3d");
+            AssetBundle xmlBundle = new AssetBundle(cardXml);
+            var extractor = new TextExtractor();
+            foreach (var obj in xmlBundle.Objects)
+            {
+                extractor.Extract(obj, _outDir);
+            }
+            // Create cardDB from english xml
+            var xmlFile = Path.Combine(_outDir, "cardxml0", "TextAsset", "enUS.xml");
+            if (File.Exists(xmlFile))
+            {
+                CardDb.Read(xmlFile);
+                cardDb = CardDb.All;
+                Logger.Log("CardDB loaded: {0} cards", cardDb.Count);
+            }
+            else
+            {
+                Logger.Log(LogLevel.ERROR, "Card XML file not extracted: {0}", xmlFile);
+            }
+            return cardDb;
+        }
+    }
+}
