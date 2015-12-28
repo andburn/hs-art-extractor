41b37cd240efded24e99be3256aa5912e1964874
Andrew Burnett <andrew@nospam.forme>
2015-12-28 16:17:27 +0000

    Add some debugging to cli

--- HearthstoneDisunityCLI/Program.cs
+++ HearthstoneDisunityCLI/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using HearthstoneDisunity;
 using HearthstoneDisunity.Hearthstone;
+using HearthstoneDisunity.Hearthstone.CardArt;
 using HearthstoneDisunity.Hearthstone.Database;
+using HearthstoneDisunity.Util;
 
 namespace HearthstoneDisunityCLI
 {
@@ -14,7 +17,9 @@ namespace HearthstoneDisunityCLI
 
         private static void Main(string[] args)
         {
-            _usage = UsuageText();
+            Logger.SetLogLevel(LogLevel.DEBUG);
+
+            _usage = UsageText();
 
             if (args.Length < 1)
             {
@@ -54,6 +59,7 @@ namespace HearthstoneDisunityCLI
             var file = args[1];
             var dir = args[2];
 
+            Logger.SetLogLocation(dir);
             Console.WriteLine("Extracting texture assets from {0} to {1}", file, dir);
             try
             {
@@ -73,6 +79,7 @@ namespace HearthstoneDisunityCLI
             var file = args[1];
             var dir = args[2];
 
+            Logger.SetLogLocation(dir);
             Console.WriteLine("Dumping all assets from {0} to {1}", file, dir);
             try
             {
@@ -99,19 +106,47 @@ namespace HearthstoneDisunityCLI
                 hsDir = args[1];
                 outDir = args[2];
             }
+
             if (args.Length >= 4)
             {
                 hsDir = args[1];
                 outDir = args[2];
                 var success = int.TryParse(args[3], out set);
                 if (success == false)
-                    set = -1;
+                {
+                    try
+                    {
+                        CardSet setname = (CardSet)Enum.Parse(typeof(CardSet), args[3], true);
+                        set = (int)setname;
+                    }
+                    catch
+                    {
+                        set = -1;
+                    }
+                }
             }
 
+            Logger.SetLogLocation(outDir);
             Console.WriteLine("Extracting card art from {0} to {1}", hsDir, outDir);
             try
             {
                 Extract.CardArt(outDir, hsDir, set);
+                // TODO: debug
+                // check extracted full cards (name = id) against cardb
+                if (set > 0)
+                {
+                    List<string> ids = new List<string>();
+                    var files = Directory.GetFiles(Path.Combine(outDir, "Full"));
+                    foreach (var f in files)
+                    {
+                        ids.Add(StringUtils.GetFilenameNoExt(f));
+                    }
+                    var notfound = CardDb.All.Values.Where(x => x.Set == (CardSet)set).Where(x => !ids.Contains(x.Id));
+                    foreach (var nf in notfound)
+                    {
+                        Console.WriteLine(nf.Name + ", " + nf.Id);
+                    }
+                }
             }
             catch (Exception e)
             {
@@ -127,12 +162,22 @@ namespace HearthstoneDisunityCLI
             var hsDir = args[1];
             var outDir = args[2];
 
+            Logger.SetLogLocation(outDir);
             Console.WriteLine("Generating card counts from cardxml");
             try
             {
-                var cardxml = Path.Combine(hsDir, "cardxml0.unity3d");
-                Extract.Text(outDir, cardxml);
-                var cardtxt = Path.Combine(outDir, "TextAsset", "enUS.txt");
+                var cardtxt = Path.Combine(outDir, "cardxml0", "TextAsset", "enUS.txt");
+                var artxml = Path.Combine(outDir, "CardArtDefs.xml");
+                if (!File.Exists(cardtxt))
+                {
+                    // extract text if it doesn't exist
+                    var cardxml = Path.Combine(hsDir, "Data", "Win", "cardxml0.unity3d");
+                    Extract.Text(outDir, cardxml);
+                }
+                if (File.Exists(artxml))
+                {
+                    CardArtDb.Read(artxml);
+                }
                 CardDb.Read(cardtxt);
                 PrintCardCounts();
             }
@@ -154,7 +199,7 @@ namespace HearthstoneDisunityCLI
             Environment.Exit(1);
         }
 
-        private static string UsuageText()
+        private static string UsageText()
         {
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("Usage:");
@@ -167,36 +212,36 @@ namespace HearthstoneDisunityCLI
         private static void PrintCardCounts()
         {
             var cards = CardDb.All.Values.ToList<Card>();
+            var cardart = CardArtDb.All.Values.ToList<ArtCard>();
 
-            Console.WriteLine("\nTotals:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count, cards.Count(x => x.IsCollectible && x.Type != CardType.HERO));
-            Console.WriteLine("Basic:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count(x => x.Set == CardSet.CORE),
-                cards.Count(x => x.IsCollectible && x.Set == CardSet.CORE && x.Type != CardType.HERO));
-            Console.WriteLine("Classic:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count(x => x.Set == CardSet.EXPERT1),
-                cards.Count(x => x.IsCollectible && x.Set == CardSet.EXPERT1));
-            Console.WriteLine("Naxx:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count(x => x.Set == CardSet.FP1),
-                cards.Count(x => x.IsCollectible && x.Set == CardSet.FP1));
-            Console.WriteLine("BRM:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count(x => x.Set == CardSet.BRM),
-                cards.Count(x => x.IsCollectible && x.Set == CardSet.BRM));
-            Console.WriteLine("LOE:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count(x => x.Set == CardSet.LOE),
-                cards.Count(x => x.IsCollectible && x.Set == CardSet.LOE));
-            Console.WriteLine("TGT:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count(x => x.Set == CardSet.TGT),
-                cards.Count(x => x.IsCollectible && x.Set == CardSet.TGT));
-            Console.WriteLine("GVG:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count(x => x.Set == CardSet.GVG),
-                cards.Count(x => x.IsCollectible && x.Set == CardSet.GVG));
-            Console.WriteLine("Reward:\n\tall: {0}\n\tcollectible: {1}",
-                cards.Count(x => x.Set == CardSet.REWARD || x.Set == CardSet.PROMO),
-                cards.Count(x => x.IsCollectible && (x.Set == CardSet.REWARD || x.Set == CardSet.PROMO)));
+            Console.WriteLine("\nTotals:\n\tall: {0}, art: {1}\n\tcollectible: {2}\n\tcardbars: {3}",
+                cards.Count, cardart.Count,
+                cards.Count(x => x.IsCollectible && x.Type != CardType.HERO),
+                cardart.Count(x => x.GetMaterial(MaterialType.CardBar) != null));
+
+            PrintSetCount(cards, cardart, "Basic", CardSet.CORE);
+            PrintSetCount(cards, cardart, "Classic", CardSet.EXPERT1);
+            PrintSetCount(cards, cardart, "Naxx", CardSet.FP1);
+            PrintSetCount(cards, cardart, "BRM", CardSet.BRM);
+            PrintSetCount(cards, cardart, "LOE", CardSet.LOE);
+            //cards.Where(x => x.IsCollectible && x.Set == CardSet.LOE).ToList().ForEach(t => Console.WriteLine("\t" + t.Id + ", " + t.Name));
+            PrintSetCount(cards, cardart, "TGT", CardSet.TGT);
+            PrintSetCount(cards, cardart, "GVG", CardSet.GVG);
+            PrintSetCount(cards, cardart, "Reward", CardSet.PROMO, CardSet.REWARD);
 
             var filtered = CardDb.Filtered.Values.ToList<Card>();
             Console.WriteLine("\nFiltered: {0}", filtered.Count);
+        }
+
+        private static void PrintSetCount(List<Card> cards, List<ArtCard> artcards, string label, params CardSet[] sets)
+        {
+            Console.WriteLine("{0}:\n\tall: {1}\n\tcollectible: {2}\n\tcardbars: {3}",
+                label,
+                cards.Count(x => sets.Contains(x.Set)),
+                cards.Count(x => x.IsCollectible && sets.Contains(x.Set) && x.Type != CardType.HERO),
+                artcards.Count(x => x.GetMaterial(MaterialType.CardBar) != null
+                    && (sets.Contains(CardDb.All[x.Id].Set))
+                ));
         }
     }
 }
