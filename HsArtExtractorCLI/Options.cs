a1afdc511040d5eef17ddf0b4cbbe4f39230f337
Andrew Burnett <andrew@nospam.forme>
2016-01-27 15:20:11 +0000

    Setup CLI to use CommandLine library

---
+++ HsArtExtractorCLI/Options.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using CommandLine;
+
+namespace HsArtExtractorCLI
+{
+	[Verb("dump", HelpText = "Dump assets from unity3d files.")]
+	internal class DumpOptions
+	{
+		// default --verbose, no short -v
+		[Option(
+			HelpText = "Show detailed information.")]
+		public bool Verbose { get; set; }
+
+		[Value(0,
+			MetaName = "input files",
+			HelpText = "Input files to be processed.",
+			Required = true)]
+		public IEnumerable<string> InputFiles { get; set; }
+	}
+}
--- HsArtExtractorCLI/Program.cs
+++ HsArtExtractorCLI/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using CommandLine;
 using HsArtExtractor;
 using HsArtExtractor.Hearthstone;
 using HsArtExtractor.Hearthstone.CardArt;
@@ -12,303 +13,324 @@ using HsArtExtractor.Util;
 
 namespace HsArtExtractorCLI
 {
-    internal class Program
-    {
-        private static string _usage;
+	internal class Program
+	{
+		private static int Main(string[] args)
+		{
+			var result = CommandLine.Parser.Default.ParseArguments<DumpOptions>(args);
+			var exitCode = result.MapResult(
+				options =>
+				{
+					if (options.Verbose)
+						Console.WriteLine("Filenames: {0}", string.Join(",", options.InputFiles.ToArray()));
+					return 0;
+				},
+				errors =>
+				{
+					Logger.Log(errors);
+					return 1;
+				});
+			return exitCode;
+		}
+	}
 
-        private static void Main(string[] args)
-        {
-            Logger.SetLogLevel(LogLevel.DEBUG);
+	internal class ProgramOld
+	{
+		private static string _usage;
 
-            _usage = UsageText();
+		private static void MainOld(string[] args)
+		{
+			Logger.SetLogLevel(LogLevel.DEBUG);
 
-            if (args.Length < 1)
-            {
-                PrintUsageAndExit();
-            }
+			_usage = UsageText();
 
-            var command = args[0];
-            switch (command.ToLower())
-            {
-                case "textures":
-                    ExtractTextures(args);
-                    break;
+			if (args.Length < 1)
+			{
+				PrintUsageAndExit();
+			}
 
-                case "dump":
-                    ExtractRaw(args);
-                    break;
+			var command = args[0];
+			switch (command.ToLower())
+			{
+				case "textures":
+					ExtractTextures(args);
+					break;
 
-                case "cardart":
-                    CardArt(args);
-                    break;
+				case "dump":
+					ExtractRaw(args);
+					break;
 
-                case "query":
-                    QueryCardDb(args);
-                    break;
+				case "cardart":
+					CardArt(args);
+					break;
 
-                case "test":
-                    Test(args);
-                    break;
+				case "query":
+					QueryCardDb(args);
+					break;
 
-                default:
-                    PrintUsageAndExit();
-                    break;
-            }
-        }
+				case "test":
+					Test(args);
+					break;
 
-        private static void ExtractTextures(string[] args)
-        {
-            if (args.Length != 3)
-                PrintUsageAndExit();
+				default:
+					PrintUsageAndExit();
+					break;
+			}
+		}
 
-            var file = args[1];
-            var dir = args[2];
+		private static void ExtractTextures(string[] args)
+		{
+			if (args.Length != 3)
+				PrintUsageAndExit();
 
-            Logger.SetLogLocation(dir);
-            Console.WriteLine("Extracting texture assets from {0} to {1}", file, dir);
-            try
-            {
-                Extract.Textures(dir, file);
-            }
-            catch (Exception e)
-            {
-                PrintErrorAndExit(e.Message);
-            }
-        }
+			var file = args[1];
+			var dir = args[2];
 
-        private static void ExtractRaw(string[] args)
-        {
-            if (args.Length != 3)
-                PrintUsageAndExit();
+			Logger.SetLogLocation(dir);
+			Console.WriteLine("Extracting texture assets from {0} to {1}", file, dir);
+			try
+			{
+				Extract.Textures(dir, file);
+			}
+			catch (Exception e)
+			{
+				PrintErrorAndExit(e.Message);
+			}
+		}
 
-            var file = args[1];
-            var dir = args[2];
+		private static void ExtractRaw(string[] args)
+		{
+			if (args.Length != 3)
+				PrintUsageAndExit();
 
-            Logger.SetLogLocation(dir);
-            Console.WriteLine("Dumping all assets from {0} to {1}", file, dir);
-            try
-            {
-                Extract.Raw(dir, file);
-            }
-            catch (Exception e)
-            {
-                PrintErrorAndExit(e.Message);
-            }
-        }
+			var file = args[1];
+			var dir = args[2];
 
-        private static void CardArt(string[] args)
-        {
-            string hsDir = "";
-            string outDir = "";
-            int set = -1;
+			Logger.SetLogLocation(dir);
+			Console.WriteLine("Dumping all assets from {0} to {1}", file, dir);
+			try
+			{
+				Extract.Raw(dir, file);
+			}
+			catch (Exception e)
+			{
+				PrintErrorAndExit(e.Message);
+			}
+		}
 
-            if (args.Length < 3)
-            {
-                PrintUsageAndExit();
-            }
-            else if (args.Length >= 3)
-            {
-                hsDir = args[1];
-                outDir = args[2];
-            }
+		private static void CardArt(string[] args)
+		{
+			string hsDir = "";
+			string outDir = "";
+			int set = -1;
 
-            if (args.Length >= 4)
-            {
-                hsDir = args[1];
-                outDir = args[2];
-                var success = int.TryParse(args[3], out set);
-                if (success == false)
-                {
-                    try
-                    {
-                        CardSet setname = (CardSet)Enum.Parse(typeof(CardSet), args[3], true);
-                        set = (int)setname;
-                    }
-                    catch
-                    {
-                        set = -1;
-                    }
-                }
-            }
+			if (args.Length < 3)
+			{
+				PrintUsageAndExit();
+			}
+			else if (args.Length >= 3)
+			{
+				hsDir = args[1];
+				outDir = args[2];
+			}
 
-            Logger.SetLogLocation(outDir);
-            Console.WriteLine("Extracting card art from {0} to {1}", hsDir, outDir);
-            try
-            {
-                Extract.CardArt(outDir, hsDir, set);
-                // TODO: Debug only, remove
-                // check extracted full cards (name = id) against cardb
-                if (set > 0)
-                {
-                    List<string> ids = new List<string>();
-                    var files = Directory.GetFiles(Path.Combine(outDir, "Full"));
-                    foreach (var f in files)
-                    {
-                        ids.Add(StringUtils.GetFilenameNoExt(f));
-                    }
-                    var notfound = CardDb.All.Values.Where(x => x.Set == (CardSet)set).Where(x => !ids.Contains(x.Id));
-                    foreach (var nf in notfound)
-                    {
-                        Console.WriteLine(nf.Name + ", " + nf.Id);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                PrintErrorAndExit(e.Message);
-            }
-        }
+			if (args.Length >= 4)
+			{
+				hsDir = args[1];
+				outDir = args[2];
+				var success = int.TryParse(args[3], out set);
+				if (success == false)
+				{
+					try
+					{
+						CardSet setname = (CardSet)Enum.Parse(typeof(CardSet), args[3], true);
+						set = (int)setname;
+					}
+					catch
+					{
+						set = -1;
+					}
+				}
+			}
 
-        private static void QueryCardDb(string[] args)
-        {
-            if (args.Length != 3)
-                PrintUsageAndExit();
+			Logger.SetLogLocation(outDir);
+			Console.WriteLine("Extracting card art from {0} to {1}", hsDir, outDir);
+			try
+			{
+				Extract.CardArt(outDir, hsDir, set);
+				// TODO: Debug only, remove
+				// check extracted full cards (name = id) against cardb
+				if (set > 0)
+				{
+					List<string> ids = new List<string>();
+					var files = Directory.GetFiles(Path.Combine(outDir, "Full"));
+					foreach (var f in files)
+					{
+						ids.Add(StringUtils.GetFilenameNoExt(f));
+					}
+					var notfound = CardDb.All.Values.Where(x => x.Set == (CardSet)set).Where(x => !ids.Contains(x.Id));
+					foreach (var nf in notfound)
+					{
+						Console.WriteLine(nf.Name + ", " + nf.Id);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				PrintErrorAndExit(e.Message);
+			}
+		}
 
-            var hsDir = args[1];
-            var outDir = args[2];
+		private static void QueryCardDb(string[] args)
+		{
+			if (args.Length != 3)
+				PrintUsageAndExit();
 
-            Logger.SetLogLocation(outDir);
-            Console.WriteLine("Generating card counts from cardxml");
-            try
-            {
-                var cardtxt = Path.Combine(outDir, "cardxml0", "TextAsset", "enUS.txt");
-                var artxml = Path.Combine(outDir, "CardArtDefs.xml");
-                if (!File.Exists(cardtxt))
-                {
-                    // extract text if it doesn't exist
-                    var cardxml = Path.Combine(hsDir, "Data", "Win", "cardxml0.unity3d");
-                    Extract.Text(outDir, cardxml);
-                }
-                if (File.Exists(artxml))
-                {
-                    CardArtDb.Read(artxml);
-                }
-                CardDb.Read(cardtxt);
-                PrintCardCounts();
-            }
-            catch (Exception e)
-            {
-                PrintErrorAndExit(e.Message);
-            }
-        }
+			var hsDir = args[1];
+			var outDir = args[2];
 
-        private static void Test(string[] args)
-        {
-            string hsDir = "";
-            string outDir = "";
+			Logger.SetLogLocation(outDir);
+			Console.WriteLine("Generating card counts from cardxml");
+			try
+			{
+				var cardtxt = Path.Combine(outDir, "cardxml0", "TextAsset", "enUS.txt");
+				var artxml = Path.Combine(outDir, "CardArtDefs.xml");
+				if (!File.Exists(cardtxt))
+				{
+					// extract text if it doesn't exist
+					var cardxml = Path.Combine(hsDir, "Data", "Win", "cardxml0.unity3d");
+					Extract.Text(outDir, cardxml);
+				}
+				if (File.Exists(artxml))
+				{
+					CardArtDb.Read(artxml);
+				}
+				CardDb.Read(cardtxt);
+				PrintCardCounts();
+			}
+			catch (Exception e)
+			{
+				PrintErrorAndExit(e.Message);
+			}
+		}
 
-            if (args.Length < 3)
-            {
-                PrintUsageAndExit();
-            }
-            else if (args.Length >= 3)
-            {
-                hsDir = args[1];
-                outDir = args[2];
-            }
+		private static void Test(string[] args)
+		{
+			string hsDir = "";
+			string outDir = "";
 
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            CardArt(args);
-            sw.Stop();
+			if (args.Length < 3)
+			{
+				PrintUsageAndExit();
+			}
+			else if (args.Length >= 3)
+			{
+				hsDir = args[1];
+				outDir = args[2];
+			}
 
-            // Check cardxml was written
-            var textDir = Path.Combine(outDir, "TextAsset");
-            if (Directory.Exists(textDir))
-            {
-                var textFiles = Directory.GetFiles(textDir, "*.txt");
-                if (textFiles.Length == 14)
-                    Console.WriteLine("CardXml extracted.");
-                else
-                    Console.WriteLine("CardXml failed.");
-            }
-            else
-                Console.WriteLine("CardXml dir failed.");
+			Stopwatch sw = new Stopwatch();
+			sw.Start();
+			CardArt(args);
+			sw.Stop();
 
-            // Check full images were written
-            var fullDir = Path.Combine(outDir, "Full");
-            if (Directory.Exists(textDir))
-            {
-                var imgFiles = Directory.GetFiles(fullDir, "*.png");
-                if (imgFiles.Length == 1875)
-                    Console.WriteLine("Full images extracted.");
-                else
-                    Console.WriteLine("Full images failed.");
-            }
-            else
-                Console.WriteLine("Full images dir failed.");
+			// Check cardxml was written
+			var textDir = Path.Combine(outDir, "TextAsset");
+			if (Directory.Exists(textDir))
+			{
+				var textFiles = Directory.GetFiles(textDir, "*.txt");
+				if (textFiles.Length == 14)
+					Console.WriteLine("CardXml extracted.");
+				else
+					Console.WriteLine("CardXml failed.");
+			}
+			else
+				Console.WriteLine("CardXml dir failed.");
 
-            // Check bar images were written
-            var barDir = Path.Combine(outDir, "Bars");
-            if (Directory.Exists(textDir))
-            {
-                var imgFiles = Directory.GetFiles(barDir, "*.png");
-                if (imgFiles.Length == 916)
-                    Console.WriteLine("Bar images extracted.");
-                else
-                    Console.WriteLine("Bar images failed.");
-            }
-            else
-                Console.WriteLine("Bar images dir failed.");
+			// Check full images were written
+			var fullDir = Path.Combine(outDir, "Full");
+			if (Directory.Exists(textDir))
+			{
+				var imgFiles = Directory.GetFiles(fullDir, "*.png");
+				if (imgFiles.Length == 1875)
+					Console.WriteLine("Full images extracted.");
+				else
+					Console.WriteLine("Full images failed.");
+			}
+			else
+				Console.WriteLine("Full images dir failed.");
 
-            // Extraction time
-            Console.WriteLine("Extraction time: {0}", sw.Elapsed);
-        }
+			// Check bar images were written
+			var barDir = Path.Combine(outDir, "Bars");
+			if (Directory.Exists(textDir))
+			{
+				var imgFiles = Directory.GetFiles(barDir, "*.png");
+				if (imgFiles.Length == 916)
+					Console.WriteLine("Bar images extracted.");
+				else
+					Console.WriteLine("Bar images failed.");
+			}
+			else
+				Console.WriteLine("Bar images dir failed.");
 
-        private static void PrintErrorAndExit(string message)
-        {
-            Console.WriteLine("Error: " + message);
-            Environment.Exit(1);
-        }
+			// Extraction time
+			Console.WriteLine("Extraction time: {0}", sw.Elapsed);
+		}
 
-        private static void PrintUsageAndExit()
-        {
-            Console.WriteLine(_usage);
-            Environment.Exit(1);
-        }
+		private static void PrintErrorAndExit(string message)
+		{
+			Console.WriteLine("Error: " + message);
+			Environment.Exit(1);
+		}
 
-        private static string UsageText()
-        {
-            StringBuilder sb = new StringBuilder();
-            sb.AppendLine("Usage:");
-            sb.AppendLine("\tcardart <hs_dir> <output_dir>");
-            sb.AppendLine("\ttextures <file> <output_dir>");
-            sb.AppendLine("\tdump <file> <output_dir>");
-            return sb.ToString();
-        }
+		private static void PrintUsageAndExit()
+		{
+			Console.WriteLine(_usage);
+			Environment.Exit(1);
+		}
 
-        private static void PrintCardCounts()
-        {
-            var cards = CardDb.All.Values.ToList<Card>();
-            var cardart = CardArtDb.All.Values.ToList<ArtCard>();
+		private static string UsageText()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine("Usage:");
+			sb.AppendLine("\tcardart <hs_dir> <output_dir>");
+			sb.AppendLine("\ttextures <file> <output_dir>");
+			sb.AppendLine("\tdump <file> <output_dir>");
+			return sb.ToString();
+		}
 
-            Console.WriteLine("\nTotals:\n\tall: {0}, art: {1}\n\tcollectible: {2}\n\tcardbars: {3}",
-                cards.Count, cardart.Count,
-                cards.Count(x => x.IsCollectible && x.Type != CardType.HERO),
-                cardart.Count(x => x.GetMaterial(MaterialType.CardBar) != null));
+		private static void PrintCardCounts()
+		{
+			var cards = CardDb.All.Values.ToList<Card>();
+			var cardart = CardArtDb.All.Values.ToList<ArtCard>();
 
-            PrintSetCount(cards, cardart, "Basic", CardSet.CORE);
-            PrintSetCount(cards, cardart, "Classic", CardSet.EXPERT1);
-            PrintSetCount(cards, cardart, "Naxx", CardSet.FP1);
-            PrintSetCount(cards, cardart, "BRM", CardSet.BRM);
-            PrintSetCount(cards, cardart, "LOE", CardSet.LOE);
-            PrintSetCount(cards, cardart, "TGT", CardSet.TGT);
-            PrintSetCount(cards, cardart, "GVG", CardSet.GVG);
-            PrintSetCount(cards, cardart, "Reward", CardSet.PROMO, CardSet.REWARD);
+			Console.WriteLine("\nTotals:\n\tall: {0}, art: {1}\n\tcollectible: {2}\n\tcardbars: {3}",
+				cards.Count, cardart.Count,
+				cards.Count(x => x.IsCollectible && x.Type != CardType.HERO),
+				cardart.Count(x => x.GetMaterial(MaterialType.CardBar) != null));
 
-            var filtered = CardDb.Filtered.Values.ToList<Card>();
-            Console.WriteLine("\nFiltered: {0}", filtered.Count);
-        }
+			PrintSetCount(cards, cardart, "Basic", CardSet.CORE);
+			PrintSetCount(cards, cardart, "Classic", CardSet.EXPERT1);
+			PrintSetCount(cards, cardart, "Naxx", CardSet.FP1);
+			PrintSetCount(cards, cardart, "BRM", CardSet.BRM);
+			PrintSetCount(cards, cardart, "LOE", CardSet.LOE);
+			PrintSetCount(cards, cardart, "TGT", CardSet.TGT);
+			PrintSetCount(cards, cardart, "GVG", CardSet.GVG);
+			PrintSetCount(cards, cardart, "Reward", CardSet.PROMO, CardSet.REWARD);
 
-        private static void PrintSetCount(List<Card> cards, List<ArtCard> artcards, string label, params CardSet[] sets)
-        {
-            Console.WriteLine("{0}:\n\tall: {1}\n\tcollectible: {2}\n\tcardbars: {3}",
-                label,
-                cards.Count(x => sets.Contains(x.Set)),
-                cards.Count(x => x.IsCollectible && sets.Contains(x.Set) && x.Type != CardType.HERO),
-                artcards.Count(x => x.GetMaterial(MaterialType.CardBar) != null
-                    && (sets.Contains(CardDb.All[x.Id].Set))
-                ));
-        }
-    }
+			var filtered = CardDb.Filtered.Values.ToList<Card>();
+			Console.WriteLine("\nFiltered: {0}", filtered.Count);
+		}
+
+		private static void PrintSetCount(List<Card> cards, List<ArtCard> artcards, string label, params CardSet[] sets)
+		{
+			Console.WriteLine("{0}:\n\tall: {1}\n\tcollectible: {2}\n\tcardbars: {3}",
+				label,
+				cards.Count(x => sets.Contains(x.Set)),
+				cards.Count(x => x.IsCollectible && sets.Contains(x.Set) && x.Type != CardType.HERO),
+				artcards.Count(x => x.GetMaterial(MaterialType.CardBar) != null
+					&& (sets.Contains(CardDb.All[x.Id].Set))
+				));
+		}
+	}
 }
