d84dcf61a41804be118d859202601424f06ff1f4
Andrew Burnett <andrew@nospam.forme>
2016-01-30 16:07:12 +0000

    Fix text asset extraction

--- HsArtExtractor/Unity/Objects/TextAsset.cs
+++ HsArtExtractor/Unity/Objects/TextAsset.cs
@@ -1,30 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using HsArtExtractor.Util;
 
 namespace HsArtExtractor.Unity.Objects
 {
-    public class TextAsset
-    {
-        public string Name { get; set; }
-        private byte[] _text;
+	public class TextAsset
+	{
+		public string Name { get; set; }
+		private byte[] _text;
 
-        public TextAsset(BinaryBlock b)
-        {
-            var nameLen = b.ReadInt();
-            Name = b.ReadFixedString(nameLen);
-            var textLen = b.ReadInt();
-            _text = b.ReadBytes(textLen); // TODO: overflow possible?
-        }
+		public TextAsset(BinaryBlock b)
+		{
+			var nameLen = b.ReadInt();
+			Name = b.ReadFixedString(nameLen);
+			b.Align(4);
+			var textLen = b.ReadInt();
+			_text = b.ReadBytes(textLen);
+		}
 
-        public void Save(string dir)
-        {
-            if (_text != null)
-                File.WriteAllBytes(Path.Combine(dir, Name + ".txt"), _text);
-        }
-    }
+		public void Save(string dir)
+		{
+			if (_text != null)
+				File.WriteAllBytes(Path.Combine(dir, Name + ".txt"), _text);
+		}
+	}
 }
