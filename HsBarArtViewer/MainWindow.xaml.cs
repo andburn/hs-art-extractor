f5a739c796270f477b758b384f48df380bee2b81
Andrew Burnett <andrew@nospam.forme>
2016-02-10 17:24:13 +0000

    Add loading of files from directory

--- HsBarArtViewer/MainWindow.xaml.cs
+++ HsBarArtViewer/MainWindow.xaml.cs
@@ -1,34 +1,44 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-
+using HsArtExtractor.Hearthstone.CardArt;
+using HsArtExtractor.Util;
+using ControlsImage = System.Windows.Controls.Image;
 using FormsDialogResult = System.Windows.Forms.DialogResult;
+using WindowsPoint = System.Windows.Point;
 
 namespace HsBarArtViewer
 {
 	public partial class MainWindow : Window
 	{
-		// Determine if we're presently dragging
 		private static bool _isDragging = false;
 
 		// The offset from the top, left of the item being dragged
 		// and the original mouse down
-		private static Point _offset;
+		private static WindowsPoint _offset;
 
-		private Image draggedImage;
+		private ControlsImage _draggedImage;
+		private BitmapImage _original;
 
 		// For zoom in/out
 		private double _zoomPercent;
 
-		private BitmapImage _original;
+		// File list
+		private string[] _fileList;
+
+		// Current file index in the list
+		private int _fileIndex;
 
 		public MainWindow()
 		{
 			_zoomPercent = 1;
+			_fileIndex = 0;
 			InitializeComponent();
 		}
 
@@ -39,7 +49,30 @@ namespace HsBarArtViewer
 			var dialog = new FolderBrowserDialog();
 			var result = dialog.ShowDialog();
 			if (result == FormsDialogResult.OK)
-				StatusWrite("Browse: " + dialog.SelectedPath);
+			{
+				StatusWrite("Loading from " + dialog.SelectedPath);
+				_fileList = Directory.GetFiles(dialog.SelectedPath, "*.png");
+				LoadFile();
+			}
+		}
+
+		private void BtnMapBrowse_Click(object sender, RoutedEventArgs e)
+		{
+			StatusWrite("MapBrowse Clicked");
+			var dialog = new OpenFileDialog();
+			dialog.DefaultExt = ".xml";
+			dialog.Filter = "XML Files (*.xml)|*.xml";
+
+			var result = dialog.ShowDialog();
+			if (result == FormsDialogResult.OK)
+			{
+				string filename = dialog.FileName;
+				StatusWrite("MapBrowse: " + filename);
+				// read db info from file
+				// TODO: handle exceptions?
+				CardArtDb.Read(filename);
+				LoadFile();
+			}
 		}
 
 		private void BtnCalculate_Click(object sender, RoutedEventArgs e)
@@ -50,20 +83,23 @@ namespace HsBarArtViewer
 		private void BtnReset_Click(object sender, RoutedEventArgs e)
 		{
 			StatusWrite("Reset Clicked");
-			_zoomPercent = 1;
-			_offset = new Point(0, 0);
-			ImgBase.SetValue(Canvas.LeftProperty, _offset.X);
-			ImgBase.SetValue(Canvas.TopProperty, _offset.Y);
+			ResetView();
 		}
 
 		private void BtnPrevious_Click(object sender, RoutedEventArgs e)
 		{
 			StatusWrite("Previous Clicked");
+			if (_fileList != null && _fileIndex - 1 >= 0)
+				_fileIndex -= 1;
+			LoadFile();
 		}
 
 		private void BtnNext_Click(object sender, RoutedEventArgs e)
 		{
 			StatusWrite("Next Clicked");
+			if (_fileList != null && _fileIndex + 1 < _fileList.Length)
+				_fileIndex += 1;
+			LoadFile();
 		}
 
 		// Canvas image movement handlers
@@ -81,7 +117,7 @@ namespace HsBarArtViewer
 			if (ImgBase == null)
 				return;
 
-			draggedImage = ImgBase;
+			_draggedImage = ImgBase;
 		}
 
 		private void Canvas_MouseLeftUp(object sender, MouseButtonEventArgs e)
@@ -100,14 +136,14 @@ namespace HsBarArtViewer
 				return;
 
 			// Get the position of the mouse relative to the canvas
-			Point mousePoint = e.GetPosition(CnvMain);
+			WindowsPoint mousePoint = e.GetPosition(CnvMain);
 
 			// Offset the mouse position by the original offset position
 			mousePoint.Offset(-_offset.X, -_offset.Y);
 
 			// Move the element on the canvas
-			draggedImage.SetValue(Canvas.LeftProperty, mousePoint.X);
-			draggedImage.SetValue(Canvas.TopProperty, mousePoint.Y);
+			_draggedImage.SetValue(Canvas.LeftProperty, mousePoint.X);
+			_draggedImage.SetValue(Canvas.TopProperty, mousePoint.Y);
 		}
 
 		private void Canvas_MouseWheel(object sender, MouseWheelEventArgs e)
@@ -143,6 +179,48 @@ namespace HsBarArtViewer
 		private void StatusWrite(string text)
 		{
 			tbStatus.Text += $"{text}\n";
+		}
+
+		private void ResetView()
+		{
+			_zoomPercent = 1;
+			_offset = new WindowsPoint(0, 0);
+			if (_original != null)
+				ImgBase.Source = _original;
+			ImgBase.SetValue(Canvas.LeftProperty, _offset.X);
+			ImgBase.SetValue(Canvas.TopProperty, _offset.Y);
+		}
+
+		private void LoadFile()
+		{
+			if (_fileList != null && _fileList.Length > 0)
+			{
+				var filename = _fileList[_fileIndex];
+				var cardId = StringUtils.GetFilenameNoExt(filename);
+				ImgBase.Source = BitmapToImageSource(new Bitmap(filename));
+				_original = (BitmapImage)ImgBase.Source; // TODO same problem as above
+				if (CardArtDb.All.ContainsKey(cardId))
+				{
+					StatusWrite(CardArtDb.All[cardId].Texture.Path);
+				}
+				ResetView();
+			}
+		}
+
+		private BitmapImage BitmapToImageSource(Bitmap bitmap)
+		{
+			using (MemoryStream memory = new MemoryStream())
+			{
+				bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+				memory.Position = 0;
+				BitmapImage bitmapimage = new BitmapImage();
+				bitmapimage.BeginInit();
+				bitmapimage.StreamSource = memory;
+				bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+				bitmapimage.EndInit();
+
+				return bitmapimage;
+			}
 		}
 	}
 }
